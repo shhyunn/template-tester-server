3eb9f2e4b0c560f7e05398ef7c40bd151559786e
sohyun lim <limgm7@gmail.com>
2026-03-01 22:45:31 +0900

    Feat: 예시 Member 관련 파일 제거

--- src/main/java/codenine/template_tester/TemplateTesterApplication.java
+++ src/main/java/codenine/template_tester/TemplateTesterApplication.java
@@ -1,13 +1,12 @@
 package codenine.template_tester;
-
-import codenine.template_tester.domain.Member;
-import codenine.template_tester.repository.MemberRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TemplateTesterApplication {
 
 	public static void main(String[] args) {
@@ -15,12 +14,12 @@ public class TemplateTesterApplication {
 	}
 
 	//서버 시작 시 등록
-	@Bean
-	CommandLineRunner run(MemberRepository memberRepository) {
-		return args -> {
-			memberRepository.save(new Member("sohyun"));
-			System.out.println("Member saved!");
-		};
-	}
+//	@Bean
+//	CommandLineRunner run(MemberRepository memberRepository) {
+//		return args -> {
+//			memberRepository.save(new Member("sohyun"));
+//			System.out.println("Member saved!");
+//		};
+//	}
 
 }
--- src/main/java/codenine/template_tester/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package codenine.template_tester.repository;
-
-import codenine.template_tester.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
